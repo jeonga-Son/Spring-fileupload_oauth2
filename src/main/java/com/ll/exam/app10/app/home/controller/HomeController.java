4824da7d0a6b7ca1e088b8401570269a6d3e91af
jeonga <jjeong0106@gmail.com>
2022-10-03 20:22:32 +0900

    현재 로그인된 회원의 프로필 이미지를 메인페이지 상단에 노출

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -1,16 +1,36 @@
 package com.ll.exam.app10.app.home.controller;
 
+import com.ll.exam.app10.app.member.entity.Member;
+import com.ll.exam.app10.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 // @RequiredArgsConstructor는 Lombok으로 스프링에서 DI(의존성 주입)의 방법 중에 생성자 주입을 임의의 코드없이 자동으로 설정해주는 어노테이션이다.
 @RequiredArgsConstructor
 public class HomeController {
+    private final MemberService memberService;
     // RequestMapping(요청 매핑)은 요청이 왔을 때 어떤 컨트롤러가 호출이 되어야 하는지 알려주는 지표 같은 것이다.
     @RequestMapping("/")
-    public String main() {
+    public String main(Principal principal, Model model) {
+        Member loginedMember = null;
+        String loginedMemberProfileImgUrl = null;
+
+        if (principal != null && principal.getName() != null) {
+            loginedMember = memberService.getMemberByUsername(principal.getName());
+        }
+
+        if (loginedMember != null) {
+            loginedMemberProfileImgUrl = loginedMember.getProfileImgUrl();
+        }
+
+        model.addAttribute("loginedMember", loginedMember);
+        model.addAttribute("loginedMemberProfileImgUrl", loginedMemberProfileImgUrl);
+
         return "home/main";
     }
 
