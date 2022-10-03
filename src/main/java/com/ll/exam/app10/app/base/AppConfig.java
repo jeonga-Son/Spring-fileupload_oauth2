fb6cad8e73a0f269ac384b33273df8f8782cf346
jeonga <jjeong0106@gmail.com>
2022-10-03 17:50:02 +0900

    회원가입 테스트 후 업로드 된 프로필이미지도 제거

---
+++ src/main/java/com/ll/exam/app10/app/base/AppConfig.java
@@ -0,0 +1,14 @@
+package com.ll.exam.app10.app.base;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    public static String GET_FILE_DIR_PATH; // static으로 하면 @Value가 안먹힌다. 따라서 아래처럼 setter를 만든다.
+
+    @Value("${custom.genFileDirPath}")
+    public void setFileDirPath(String genFileDirPath) { // @Value를 setter에 붙이면 자동으로 한번 실행된다.
+        GET_FILE_DIR_PATH = genFileDirPath;
+    };
+}
