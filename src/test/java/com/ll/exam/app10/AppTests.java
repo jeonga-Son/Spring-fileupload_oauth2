885c7bf4003623e79e9310a7cb770c4f3c00362c
jeonga <jjeong0106@gmail.com>
2022-10-03 11:51:06 +0900

    테스트 환경의 샘플데이터 생성, 테스트 DB 분리

--- src/test/java/com/ll/exam/app10/AppTests.java
+++ src/test/java/com/ll/exam/app10/AppTests.java
@@ -1,16 +1,20 @@
 package com.ll.exam.app10;
 
 import com.ll.exam.app10.app.home.controller.HomeController;
+import com.ll.exam.app10.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -21,10 +25,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 // @Transactional은 여기서 실행된 것은 실제 DB에 반영이 안되도록 하는 것 이다.
 @Transactional
+//  현재의 프로파일(환경)을 지정하도록 도와주는 어노테이션이 @ActiveProfiles 이다.
+@ActiveProfiles({"base-addi", "test"})
 class AppTests {
 
 	@Autowired
 	private MockMvc mvc;
+	@Autowired
+	private MemberService memberService;
 
 	@Test
 	@DisplayName("메인화면에서는 안녕이 나와야 한다.")
@@ -43,5 +51,13 @@ class AppTests {
 				.andExpect(handler().handlerType(HomeController.class)) // .perform(get("/"))를 수행하면 HomeController가 수형되는 것 검증.
 				.andExpect(handler().methodName("main"))
 				.andExpect(content().string(containsString("스프링부트-파일업로드 실습"))); // import할 때 client가 아닌 servlet으로 해야한다.
+	}
+
+	@Test
+	@DisplayName("회원의 수")
+	@Rollback(false)
+	void t2() {
+		long count = memberService.count();
+		assertThat(count).isGreaterThan(0);
 	}
 }
