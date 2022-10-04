9b3977c5001d7f1d62e5846d8b4cfad883d160ba
jeonga <jjeong0106@gmail.com>
2022-10-05 01:55:26 +0900

    @AuthenticationPrincipal MemberContext memberContext

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -1,8 +1,9 @@
 package com.ll.exam.app10.app.home.controller;
 
 import com.ll.exam.app10.app.member.service.MemberService;
+import com.ll.exam.app10.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,10 +27,15 @@ public class HomeController {
         return "home/about";
     }
 
-    @PreAuthorize("isAuthenticated()")
     @GetMapping("/currentUserOrigin")
     @ResponseBody
     public Principal currentUserOrigin(Principal principal) {
         return principal;
+    }
+
+    @GetMapping("/currentUser")
+    @ResponseBody
+    public MemberContext currentUser(@AuthenticationPrincipal MemberContext memberContext) {
+        return memberContext;
     }
 }
