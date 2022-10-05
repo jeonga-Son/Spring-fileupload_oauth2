de3867ebb8603e4e7fc8c609321babd705542fe4
jeonga <jjeong0106@gmail.com>
2022-10-05 18:06:08 +0900

    로거, 디버그 활성화, 카카오톡 프로필 이미지 받기

--- src/main/java/com/ll/exam/app10/app/security/service/OAuth2UserService.java
+++ src/main/java/com/ll/exam/app10/app/security/service/OAuth2UserService.java
@@ -7,6 +7,7 @@ import com.ll.exam.app10.app.member.service.MemberService;
 import com.ll.exam.app10.app.security.dto.MemberContext;
 import com.ll.exam.app10.app.security.exception.OAuthTypeMatchNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -22,11 +23,10 @@ import java.util.*;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class OAuth2UserService extends DefaultOAuth2UserService {
-
     @Autowired
     private MemberRepository memberRepository;
-
     @Autowired
     private MemberService memberService;
 
@@ -34,26 +34,36 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint()
                 .getUserNameAttributeName();
         Map<String, Object> attributes = oAuth2User.getAttributes();
+
         String oauthId = oAuth2User.getName();
+
         Member member = null;
         String oauthType = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
+
         if (!"KAKAO".equals(oauthType)) {
             throw new OAuthTypeMatchNotFoundException();
         }
+
         if (isNew(oauthType, oauthId)) {
             switch (oauthType) {
                 case "KAKAO" -> {
+                    log.debug("attributes : " + attributes);
+
                     Map attributesProperties = (Map) attributes.get("properties");
                     Map attributesKakaoAcount = (Map) attributes.get("kakao_account");
                     String nickname = (String) attributesProperties.get("nickname");
+                    String profile_image = (String) attributesProperties.get("profile_image");
                     String email = "%s@kakao.com".formatted(oauthId);
                     String username = "KAKAO_%s".formatted(oauthId);
+
                     if ((boolean) attributesKakaoAcount.get("has_email")) {
                         email = (String) attributesKakaoAcount.get("email");
                     }
+
                     member = Member.builder()
                             .email(email)
                             .username(username)
@@ -62,7 +72,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
 
                     memberRepository.save(member);
 
-                    memberService.setProfileImgByUrl(member, "https://picsum.photos/200/300");
+                    memberService.setProfileImgByUrl(member, profile_image);
                 }
             }
         } else {
@@ -78,5 +88,4 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     private boolean isNew(String oAuthType, String oAuthId) {
         return memberRepository.findByUsername("%s_%s".formatted(oAuthType, oAuthId)).isEmpty();
     }
-
 }
