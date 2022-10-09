7338cc3243badfcfe00747ad494d101c3d95d0b7
jeonga <jjeong0106@gmail.com>
2022-10-09 19:31:05 +0900

    모든 entity에 extra 필드 추가

--- src/main/java/com/ll/exam/app10/app/base/entity/BaseEntity.java
+++ src/main/java/com/ll/exam/app10/app/base/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.ll.exam.app10.app.base.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,6 +9,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -41,6 +40,10 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime modifyDate;
+
+    @Transient // 아래 필드가 DB 필드가 되는 것을 막는다.
+    @Builder.Default
+    private Map<String, Object> extra = new LinkedHashMap<>();
 
     public BaseEntity(long id) {
         this.id = id;
